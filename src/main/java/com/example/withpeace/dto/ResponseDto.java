e7087f20c4e9b6b070e8e19b159aede0efa7a733
Nachanjin <skxkswls@gmail.com>
2024-02-21 21:24:54 +0900

    fix : ReponseDto success 필드 제거

--- src/main/java/com/example/withpeace/dto/ResponseDto.java
+++ src/main/java/com/example/withpeace/dto/ResponseDto.java
@@ -12,30 +12,29 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 public record ResponseDto<T>(@JsonIgnore HttpStatus httpStatus,
-                             @NotNull Boolean success,
                              @Nullable T data,
-                             @Nullable ExceptionDto error){
+                             @Nullable ExceptionDto error) {
     public static <T> ResponseDto<T> ok(@Nullable final T data) {
-        return new ResponseDto<>(HttpStatus.OK, true, data, null);
+        return new ResponseDto<>(HttpStatus.OK, data, null);
     }
 
     public static <T> ResponseDto<T> created(@Nullable final T data) {
-        return new ResponseDto<>(HttpStatus.CREATED, true, data, null);
+        return new ResponseDto<>(HttpStatus.CREATED, data, null);
     }
 
     public static ResponseDto<Object> fail(final CommonException e) {
-        return new ResponseDto<>(e.getErrorCode().getHttpStatus(), false, null, new ExceptionDto(e.getErrorCode(), e.getMessage()));
+        return new ResponseDto<>(e.getErrorCode().getHttpStatus(), null, new ExceptionDto(e.getErrorCode(), e.getMessage()));
     }
 
     public static ResponseDto<Object> fail(final HandlerMethodValidationException e) {
-        return new ResponseDto<>(HttpStatus.BAD_REQUEST, false, null, new ExceptionDto(ErrorCode.INVALID_ARGUMENT, e.getMessage()));
+        return new ResponseDto<>(HttpStatus.BAD_REQUEST, null, new ExceptionDto(ErrorCode.INVALID_ARGUMENT, e.getMessage()));
     }
 
     public static ResponseDto<Object> fail(final MissingServletRequestParameterException e) {
-        return new ResponseDto<>(HttpStatus.BAD_REQUEST, false, null, new ExceptionDto(ErrorCode.INVALID_ARGUMENT, e.getMessage()));
+        return new ResponseDto<>(HttpStatus.BAD_REQUEST, null, new ExceptionDto(ErrorCode.INVALID_ARGUMENT, e.getMessage()));
     }
 
     public static ResponseDto<Object> fail(final MethodArgumentNotValidException e) {
-        return new ResponseDto<>(HttpStatus.BAD_REQUEST, false, null, new ExceptionDto(ErrorCode.INVALID_ARGUMENT, e.getMessage()));
+        return new ResponseDto<>(HttpStatus.BAD_REQUEST, null, new ExceptionDto(ErrorCode.INVALID_ARGUMENT, e.getMessage()));
     }
 }
