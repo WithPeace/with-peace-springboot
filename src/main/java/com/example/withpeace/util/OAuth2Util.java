a11611777f8f238040d3d6c398d690e332625662
Nachanjin <skxkswls@gmail.com>
2024-02-23 21:10:26 +0900

    fix : 구글로그인 ID_Token으로 가능하게끔 변경

--- src/main/java/com/example/withpeace/util/OAuth2Util.java
+++ src/main/java/com/example/withpeace/util/OAuth2Util.java
@@ -2,54 +2,41 @@ package com.example.withpeace.util;
 
 import com.example.withpeace.exception.CommonException;
 import com.example.withpeace.exception.ErrorCode;
-import com.nimbusds.jose.shaded.gson.JsonParser;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.springframework.security.oauth2.core.OAuth2TokenIntrospectionClaimNames.CLIENT_ID;
+
+;
 
 @Component
 @RequiredArgsConstructor
 public class OAuth2Util {
 
-    // GOOGLE 용 Data
-    @Value("${client.provider.google.authorization-uri: aaa.bbb.ccc}")
-    private String GOOGLE_AUTHORIZATION_URL;
-    @Value("${client.provider.google.token-uri: aaa.bbb.ccc}")
-    private String GOOGLE_TOKEN_URL;
-    @Value("${client.provider.google.user-info-uri: aaa.bbb.ccc}")
-    private String GOOGLE_USERINFO_URL;
+    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final HttpTransport HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
 
-    @Value("${security.oauth2.client.registration.google.client-id: aaa.bbb.ccc}")
-    private String GOOGLE_CLIENT_ID;
-    @Value("${security.oauth2.client.registration.google.client-secret: aaa.bbb.ccc}")
-    private String GOOGLE_CLIENT_SECRET;
-    @Value("${security.oauth2.client.registration.google.redirect-uri: aaa.bbb.ccc}")
-    private String GOOGLE_REDIRECT_URL;
-
-    public String getGoogleUserInformation(final String accessToken) {
-        final WebClient webClient = WebClient.builder()
-                .defaultHeaders(httpHeaders -> {
-                    httpHeaders.setBearerAuth(accessToken);
-                })
+    public String getGoogleUserInformation(final String accessToken) throws CommonException, IOException {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(HTTP_TRANSPORT, JSON_FACTORY)
+                .setAudience(Collections.singletonList(CLIENT_ID))
                 .build();
 
-        final String responseJsonBody = webClient.get()
-                .uri(GOOGLE_USERINFO_URL)
-                .retrieve()
-                .onStatus(httpStatusCode -> httpStatusCode.is4xxClientError() || httpStatusCode.is5xxServerError(),
-                        clientResponse -> {
-                            throw new CommonException(ErrorCode.AUTH_SERVER_USER_INFO_ERROR);
-                        })
-                .bodyToMono(String.class)
-                .flux()
-                .toStream()
-                .findFirst()
-                .orElseThrow(() -> new CommonException(ErrorCode.AUTH_SERVER_USER_INFO_ERROR));
+        GoogleIdToken idToken = GoogleIdToken.parse(verifier.getJsonFactory(), accessToken);
 
-        return JsonParser.parseString(responseJsonBody)
-                .getAsJsonObject()
-                .get("id")
-                .getAsString();
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+            return payload.getSubject().toString();
+        } else {
+            throw new CommonException(ErrorCode.AUTH_SERVER_USER_INFO_ERROR);
+        }
+
+
     }
 }
