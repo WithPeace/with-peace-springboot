d879f071afd0d04a69ef8bc83409001b4a81d908
lpromotion <lpromotion00@gmail.com>
2024-03-11 23:52:45 +0900

    Feat: 이미지 도메인 및 레포지토리 추가

---
+++ src/main/java/com/example/withpeace/domain/Image.java
@@ -0,0 +1,33 @@
+package com.example.withpeace.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false, unique = true)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Builder
+    public Image(Post post, String url) {
+        this.post = post;
+        this.url = url;
+    }
+}
