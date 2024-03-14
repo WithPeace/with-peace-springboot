7eac222909ca0e717fdd436945d56c2706c8a00d
Nachanjin <skxkswls@gmail.com>
2024-03-14 16:40:36 +0900

    fix : 닉네임 요구사항 변경

--- src/main/java/com/example/withpeace/dto/request/SocialRegisterRequestDto.java
+++ src/main/java/com/example/withpeace/dto/request/SocialRegisterRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SocialRegisterRequestDto(
-        @Size(min = 4, max = 10, message = "닉네임은 4자 이상 10자 이하이어야 합니다.")
+        @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하이어야 합니다.")
         @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임에 특수문자는 사용할 수 없습니다.")
         @JsonProperty("nickname") String nickname){
 }
