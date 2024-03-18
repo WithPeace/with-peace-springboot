0944f776a306713912cba31b673c44d754894efa
Nachanjin <skxkswls@gmail.com>
2024-03-18 23:05:08 +0900

    refactor : swagger 리팩토링 Jwt 적용

--- src/main/java/com/example/withpeace/dto/request/PostRegisterRequestDto.java
+++ src/main/java/com/example/withpeace/dto/request/PostRegisterRequestDto.java
@@ -2,15 +2,12 @@ package com.example.withpeace.dto.request;
 
 import com.example.withpeace.type.ETopic;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.annotation.Nullable;
-import java.util.List;
 
 public record PostRegisterRequestDto(
-        @NotBlank @JsonProperty("title") String title,
-        @NotBlank @JsonProperty("content") String content,
-        @NotNull @JsonProperty("type") ETopic type){
+        @NotBlank @JsonProperty("title") @Schema(description = "제목") String title,
+        @NotBlank @JsonProperty("content") @Schema(description = "내용") String content,
+        @NotNull @JsonProperty("type") @Schema(description = "주제") ETopic type){
 }
