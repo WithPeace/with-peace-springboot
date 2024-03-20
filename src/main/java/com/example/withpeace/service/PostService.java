3c5648ae9743f4afb058d589e38699fd9fae3ad2
lpromotion <lpromotion00@gmail.com>
2024-03-20 21:47:58 +0900

    refactor: 이미지 조회 쿼리 변경

--- src/main/java/com/example/withpeace/service/PostService.java
+++ src/main/java/com/example/withpeace/service/PostService.java
@@ -130,8 +130,7 @@ public class PostService {
 
         List<PostListResponseDto> postListResponseDtos = postPage.getContent().stream()
                 .map(post -> {
-                    String postImageUrl = imageRepository.findUrlsByPostIdOrderByIdAsc(post, imagePageable)
-                            .flatMap(urls -> urls.stream().findFirst())
+                    String postImageUrl = imageRepository.findUrlsByPostIdOrderByIdAsc(post.getId())
                             .orElse(null);
                     return PostListResponseDto.from(post, postImageUrl);
                 })
