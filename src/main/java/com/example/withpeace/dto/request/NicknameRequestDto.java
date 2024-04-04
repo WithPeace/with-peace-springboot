e029f8f24ccd0a65bdb8e60546d910af07c1cc9b
Nachanjin <skxkswls@gmail.com>
2024-04-04 20:21:29 +0900

    fix : nickname Validation 수정

--- src/main/java/com/example/withpeace/dto/request/NicknameRequestDto.java
+++ src/main/java/com/example/withpeace/dto/request/NicknameRequestDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 
 public record NicknameRequestDto(
         @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하이어야 합니다.")
-        @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임에 특수문자는 사용할 수 없습니다.")
+        @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "닉네임에 특수문자는 사용할 수 없습니다.")
         @JsonProperty("nickname")
         @Schema(description = "닉네임", example = "nickname")
         String nickname){
