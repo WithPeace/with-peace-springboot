615638181bd885e31397a8bc5cca4b2c2d6d19d7
Lee Seung Jin <lpromotion00@gmail.com>
2025-04-19 23:09:56 +0900

    refactor: 정책 리스트 조회 - 응답 DTO 로직 개선

--- src/main/java/com/example/withpeace/dto/response/PolicyListResponseDto.java
+++ src/main/java/com/example/withpeace/dto/response/PolicyListResponseDto.java
@@ -5,29 +5,34 @@ import com.example.withpeace.type.EPolicyClassification;
 import com.example.withpeace.type.EPolicyRegion;
 import lombok.Builder;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
+@Builder
 public record PolicyListResponseDto(
         String id,
         String title,
         String introduce,
         EPolicyClassification classification,
-        Set<EPolicyRegion> region,
+        List<EPolicyRegion> region,
         String ageInfo,
         String applicationPeriodStatus,
         boolean isFavorite) {
 
     public static PolicyListResponseDto from(Policy policy, boolean isFavorite) {
-        return new PolicyListResponseDto(
-                policy.getId(),
-                policy.getTitle(),
-                policy.getIntroduce(),
-                policy.getClassification(),
-                new HashSet<>(policy.getRegion()),
-                policy.getAge(),
-                policy.getApplicationPeriodStatus(),
-                isFavorite
-        );
+        return PolicyListResponseDto.builder()
+                .id(policy.getId())
+                .title(defaultIfNull(policy.getTitle()))
+                .introduce(defaultIfNull(policy.getIntroduce()))
+                .classification(policy.getClassification())
+                .region(new ArrayList<>(policy.getRegion()))
+                .ageInfo(defaultIfNull(policy.getAge()))
+                .applicationPeriodStatus(policy.getApplicationPeriodStatus())
+                .isFavorite(isFavorite)
+                .build();
+    }
+
+    private static String defaultIfNull(String value) {
+        return value != null ? value : "-";
     }
 }
